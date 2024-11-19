ddd93df7b4579fbc2fbf8496cac02cd04104da49
santiago <santiagoso1998@gmail.com>
2024-11-19 12:28:23 +0100

    fourth

--- Sprint4/Module1_SpringBoot/S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
+++ Sprint4/Module1_SpringBoot/S04T01N01/src/main/java/cat/itacademy/s04/t01/n01/controller/HelloWorldController.java
@@ -1,16 +1,13 @@
 package cat.itacademy.s04.t01.n01.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class HelloWorldController {
 
     @GetMapping("/HelloWorld")
     public String greeting(@RequestParam(defaultValue = "UNKNOWN") String name){
-        return "Hello " + name + ". You are executing a Gradle Project.";
+        return "Hello " + name + ". You are executing a Maven Project.";
     }
 
     @GetMapping({"/HelloWorld2", "/HelloWorld2/{name}"})
@@ -18,6 +15,6 @@ public class HelloWorldController {
         if (name == null){
             name = "minombre";
         }
-        return "Hello " + name + ". You are executing a Gradle Project.";
+        return "Hello " + name + ". You are executing a Maven Project.";
     }
 }
