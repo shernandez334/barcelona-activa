ea8908685000661aba3a5de565ab76db7a8079c1
santiago <santiagoso1998@gmail.com>
2024-11-21 12:50:56 +0100

    blackjack

---
+++ sprint5/S05T01_Blackjack/src/main/java/cat/itacademy/s05/t01/S05T01Application.java
@@ -0,0 +1,12 @@
+package cat.itacademy.s05.t01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01Application.class, args);
+	}
+}
---
+++ sprint5/S05T01_Blackjack/src/main/java/cat/itacademy/s05/t01/exceptions/GlobalHandleException.java
@@ -0,0 +1,5 @@
+package cat.itacademy.s05.t01.exceptions;
+
+public class GlobalHandleException {
+
+}
