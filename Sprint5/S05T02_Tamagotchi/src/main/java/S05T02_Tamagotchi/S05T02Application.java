f8d539b9feed055f63de004bb2e9fbdcd6d1c448
santiago <santiagoso1998@gmail.com>
2024-12-16 11:37:54 +0100

    fix

---
+++ Sprint5/S05T02_Tamagotchi/src/main/java/S05T02_Tamagotchi/S05T02Application.java
@@ -0,0 +1,14 @@
+package S05T02_Tamagotchi;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class S05T02Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T02Application.class, args);
+	}
+}
---
+++ Sprint5/S05T02_Tamagotchi/src/test/java/S05T02_Tamagotchi/S05T02ApplicationTests.java
@@ -0,0 +1,13 @@
+package S05T02_Tamagotchi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class S05T02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
